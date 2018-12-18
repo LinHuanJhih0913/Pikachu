4ead5ed94e1a97c7bd10c9f3a7877546a58ca60b
Jett <newjett0617@gmail.com>
2018-12-19 03:30:39 +0800

    add transaction

--- app/Http/Controllers/API/TransationController.php
+++ app/Http/Controllers/API/TransationController.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Transaction;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class TransationController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        if (!$request->bearerToken()) {
+            return response()->json([
+                'result' => 'fail',
+                'message' => 'no api in header'
+            ]);
+        }
+        $user = User::where('api_token', $request->bearerToken())->first();
+        if (!$user) {
+            return response()->json([
+                'result' => 'fail',
+                'message' => 'api_token error'
+            ]);
+        }
+        $transaction = Transaction::where('user_id', $user->id)->get(['user_id', 'game_id', 'amount']);
+        return response()->json([
+            'result' => 'success',
+            'data' => $transaction
+        ]);
+    }
 }
