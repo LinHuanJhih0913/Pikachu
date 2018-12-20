4812675dd677498700fc6a88f7e5cc3cfd9e661f
Jett <newjett0617@gmail.com>
2018-12-20 08:49:57 +0800

    fix store shop

--- app/Http/Controllers/API/ShopController.php
+++ app/Http/Controllers/API/ShopController.php
@@ -93,7 +93,7 @@ class ShopController extends Controller
                         ->name
             ]);
         }
-        if (!Shop::where('game_id', $request['game_id'])->where('item_id', $request['item_id'])->first()) {
+        if (!Shop::where('user_id', $user->id)->where('game_id', $request['game_id'])->where('item_id', $request['item_id'])->first()) {
             Shop::create([
                 'user_id' => $user->id,
                 'game_id' => $request['game_id'],
